1b584e5d79ed0455bf7e8274fee05678fbda22b9
libin1987832 <libin198783@sohu.com>
2019-10-16 06:57:25 +0000

    content:add shadowsock etc

---
+++ hexo/npmlib.sh
@@ -0,0 +1,10 @@
+node -v
+npm uninstall hexo-generator-index --save
+npm install hexo-generator-index-pin-top --save
+npm install hexo-blog-encrypt --save
+npm install hexo-admin --save
+npm install hexo-tag-plantuml --save
+#npm install imageMagick --save
+#npm install gm --save
+npm install hexo-deployer-git --save
+npm install https://github.com/threeq/hexo-tag-mermaid.git --save
