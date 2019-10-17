b1fcd136a8bfb49c8a1dc3cbf13ee57474392567
libin1987832 <libin198783@sohu.com>
2019-10-18 03:37:02 +0800

    fix:error file

--- android/label_package.sh
+++
@@ -1,6 +0,0 @@
-  FOR /F "tokens=2 delims=:=" %i IN ('adb.exe shell pm list packages -f') 
-  DO 
-  	FOR /F "tokens=1 delims=:" %j IN ('adb.exe shell /data/local/tmp/aapt-arm-pie d badging' %i)
-	DO
-		IF %j == "application-label-zh-CN" 
-			echo %k
