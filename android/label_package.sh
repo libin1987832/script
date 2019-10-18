693d8f5bb35203cc2646c5f44909e235e869f987
muzi198783 <libin1998783>
2019-10-18 13:03:05 +0800

    bug:label.sh wrong

---
+++ android/label_package.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#adb install ../other/android/ru.meefik.busybox_40.apk
+#adb push ../other/android/aapt-arm-pie /data/local/tmp/
+#adb shell "(su;busybox chown -R shell:shell /data;busybox chmod +x /data/local/tmp/aapt-arm-pie)&"
+echo "#!/bin/bash"
+for pkg in `adb shell pm list packages  -f | awk -F= '{sub("package:","");print $1}'`
+do
+  adb shell /data/local/tmp/aapt-arm-pie d badging $pkg| awk -F: '
+		$1 == "package" { split($2,space," ")
+		name=space[1];version=space[3]}
+		$1 == "application-label-zh-CN" {print "#",name,  version , $2}'       
+	echo "rm "$pkg 
+		#	$1 == "application-label" {print $2}'
+done
+#awk -F {print "rm -r" $1} | sh
