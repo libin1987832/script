cc950febf07ed238136136549ad20cbe37fcd7f8
home <home@lb.com>
2019-12-20 23:20:25 +0800

    fix:some big

--- windows/git.bat
+++ windows/git.bat
@@ -1,19 +1,18 @@
 set git="D:\Program Files\Git\bin\git" 
+H:
+
 set dir=%~dp0
 set newlog=%dir%git.log
-H:
 cd git
 for /f "tokens=1,2 delims= " %%i in (%dir%git.txt) do (
 echo cd %%i
 cd %%i
+%git% pull >> %newlog%
 echo add *.%%j >> %newlog%
-echo .>>%newlog%
 %git% add *.%%j >> %newlog%
-echo .>>%newlog%
 echo commit -m %date:~0,4%年%date:~5,2%月%date:~8,2%日 >> %newlog%
 %git% commit -m %date:~0,4%年%date:~5,2%月%date:~8,2%日 >> %newlog%
 echo push >> %newlog%
-echo .>>%newlog%
 %git% push >> %newlog%
 cd ..
 )
--- windows/schem.bat
+++ windows/schem.bat
@@ -1,6 +1,6 @@
 set dir=%~dp0
-set newfile=%dir%cmd\ip.bat
-set gitfile=%dir%cmd\git.bat
+set newfile=%dir%ip.bat
+set gitfile=%dir%git.bat
 schtasks /create /tn "Repair tencent" /tr %newfile% /sc DAILY /st 20:00
 
 schtasks /create /sc hourly /tn "git rs" /tr %newfile%
