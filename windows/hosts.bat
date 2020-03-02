f8379cecdfc9745103db51d025a2b3fd17182320
libin <libin@sohu.com>
2020-03-02 10:01:43 +0800

    2020年03月02日

---
+++ windows/hosts.bat
@@ -0,0 +1,5 @@
+@echo y|cacls "C:\Windows\System32\drivers\etc\hosts" /e /p everyone:w
+@echo\
+@echo 127.0.0.1 http://muzi198783.iok.la/ >>C:\Windows\System32\drivers\etc\hosts
+echo 修改完毕 按任意键退出~
+@pause > nul
