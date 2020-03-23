b0e33aabd7fef668554b4cfc2676b397a8013f3a
libin <libin@sohu.com>
2020-03-23 17:01:42 +0800

    2020年03月23日

--- windows/git.bat
+++ windows/git.bat
@@ -3,12 +3,16 @@ set dir=%~dp0
 set newlog=%dir%git.log
 F:
 cd code_bak\git
-for /f "tokens=1,2 delims= " %%i in (%dir%git.txt) do (
+for /f "tokens=1,2,3,4,5 delims= " %%i in (%dir%git.txt) do (
 echo enter %%i project >> %newlog%
 cd %%i
 %git% pull
 echo  *.%%j type to stage >> %newlog%
 %git% add *.%%j >> %newlog%
+if %%k==2 %git% add *.%%l >> %newlog%
+if %%k==3 (
+	%git% add *.%%l >> %newlog%
+	%git% add *.%%m >> %newlog%)
 echo commit message %date:~0,4%年%date:~5,2%月%date:~8,2%日 >> %newlog%
 echo commit reply message >>%newlog%
 %git% commit -m %date:~0,4%年%date:~5,2%月%date:~8,2%日 >> %newlog%
